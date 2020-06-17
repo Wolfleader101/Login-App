288ecb4757da13fb884a2cc410fae1184dea573c
Wolfleader <james.morgan@iinet.net.au>
2020-06-17 20:55:54 +0800

    basic app

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,25 @@
+import dotenv from "dotenv";
 import express from "express";
 import path from "path";
 import bodyParser from "body-parser";
 
-const app = express();
-const port = 3302;
+dotenv.config();
+const port = process.env.SERVER_PORT;
+
+const app: express.Application = express();
 
 app.use(bodyParser.json());
 
-app.listen(`${port}`);
-
-// tslint:disable-next-line:no-console
-console.log(`Server up and ready!\nServer on port ${port}`);
+// Configure Express to use EJS
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.send("Reply from server");
+  // render the index template
+  res.render("index");
+});
+
+app.listen(port, () => {
+  // tslint:disable-next-line:no-console
+  console.log(`Server up and ready!\nServer on port ${port}`);
 });
